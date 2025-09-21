d4ab4c963538bda4ee5bac8df45315620b0846e7
Rodrigo <76666085+RodrigoDSVDF@users.noreply.github.com>
2025-09-21 15:37:05 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' // Adicione esta linha
+import tailwindcss from '@tailwindcss/vite' // <-- Adicione esta linha
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // <-- Adicione esta linha
+  ],
   base: '/Nexus_deploy/',
-  resolve: { // Adicione esta seção inteira
+  resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
