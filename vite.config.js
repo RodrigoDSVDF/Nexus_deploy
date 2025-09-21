3f399e59cc4c3c1cb372ba6da35aa71b50ecb7ad
Rodrigo <76666085+RodrigoDSVDF@users.noreply.github.com>
2025-09-21 14:12:54 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Adicione esta linha
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Nexus_deploy/' // Esta linha é crucial!
+  base: '/Nexus_deploy/',
+  resolve: { // Adicione esta seção inteira
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
